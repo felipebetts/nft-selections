2432bab1f29238c16b0dd1cd3bb6e28626b55d24
Felipe Betts <felipe.a.betts@gmail.com>
2023-06-14 18:54:38 -0300

    feat: add error treatment middleware

---
+++ src/middleware/error.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const errorTreatment = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    })
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+}
