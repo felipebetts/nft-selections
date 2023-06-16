2102bc7710111833e46303865d65a8d6fce228be
Felipe Betts <felipe.a.betts@gmail.com>
2023-06-16 00:04:39 -0300

    feat: test open sea api

---
+++ src/utils/opensea.test.ts
@@ -0,0 +1,11 @@
+import { detailNft } from './opensea'
+
+describe('detail nft opensea api call', () => {
+  test('should return nft data', async () => {
+    const contractAddress = '0x1a92f7381b9f03921564a437210bb9396471050c'
+    const tokenId = '9888'
+    const nft = await detailNft(contractAddress, tokenId)
+    console.log('nft:', nft)
+    expect(nft).toBeDefined()
+  })
+})
